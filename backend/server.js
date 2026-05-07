aee65b98d46f242a1a6cb0ca560e0677debbe2e8
ilmannaf <ilmannafia2006@gmail.com>
2026-05-07 13:30:42 +0700

    benerin bug

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors    = require('cors');
 require('dotenv').config();
 
-const authRoutes = require('./routes/auth');
+const authRoutes = require('../routes/auth');
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
